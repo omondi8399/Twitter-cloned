05fae8c1dd6571974a9aefde83e61495fe53a36d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-24 12:08:36 +0300

    Updated Login Modal

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,55 +1,90 @@
-import useLoginModal from '@/hooks/useLoginModal'
-import React, { useCallback, useState } from 'react'
-import Input from '../Input';
-import Modal from '../Modal';
+import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
+import Input from "../Input";
+import Modal from "../Modal";
 
 const LoginModal = () => {
-    const loginModal = useLoginModal()
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-    const onSubmit = useCallback(async () => {
-        try {
-            setIsLoading(true);
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
 
+      await signIn('credentials', {
+        email,
+        password,
+      });
 
-            loginModal.onClose()
-        } catch (error) {
-            console.log(error) 
-        } finally {
-            setIsLoading(false)
-        }
-    }, [loginModal])
+      toast.success('Logged in');
 
-    const bodyContent = (
-        <div className='flex flex-col gap-4'>
-            <Input
-                placeholder='Email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                disabled={isLoading}
-            />
-            <Input
-                placeholder='Password'
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                disabled={isLoading}
-            />
-        </div>
-    )
+      loginModal.onClose();
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [email, password, loginModal]);
+
+  const onToggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal])
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input 
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}  
+      />
+      <Input 
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading} 
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>First time using Twitter?
+        <span 
+          onClick={onToggle} 
+          className="
+            text-white 
+            cursor-pointer 
+            hover:underline
+          "
+          > Create an account</span>
+      </p>
+    </div>
+  )
+
   return (
     <Modal
-        disabled={isLoading}
-        isOpen={loginModal.isOpen}
-        title="Login"
-        actionLabel="Sign in"
-        onClose={loginModal.onClose}
-        onSubmit={onSubmit}
-        body={bodyContent}
+      disabled={isLoading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Sign in"
+      onClose={loginModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+      footer={footerContent}
     />
-  )
+  );
 }
 
-export default LoginModal
+export default LoginModal;
