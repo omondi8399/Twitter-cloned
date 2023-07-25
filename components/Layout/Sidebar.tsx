cea134e958900e8940022ca311f20c4cdb42210c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-25 10:01:31 +0300

    Updated Sidebar Layout

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -10,50 +10,50 @@ import SidebarLogo from './SidebarLogo';
 import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
-const { data: currentUser } = useCurrentUser();
+  const { data: currentUser } = useCurrentUser();
 
-const items = [
+  const items = [
     {
-    icon: BsHouseFill,
-    label: 'Home',
-    href: '/',
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/',
     },
     {
-    icon: BsBellFill,
-    label: 'Notifications',
-    href: '/notifications',
-    auth: true,
-    alert: currentUser?.hasNotification
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+      alert: currentUser?.hasNotification
     },
     {
-    icon: FaUser,
-    label: 'Profile',
-    href: `/users/${currentUser?.id}`,
-    auth: true,
+      icon: FaUser,
+      label: 'Profile',
+      href: `/users/${currentUser?.id}`,
+      auth: true,
     },
-]
+  ]
 
-return (
+  return (
     <div className="col-span-1 h-full pr-4 md:pr-6">
         <div className="flex flex-col items-end">
-        <div className="space-y-2 lg:w-[230px]">
+          <div className="space-y-2 lg:w-[230px]">
             <SidebarLogo />
             {items.map((item) => (
-            <SidebarItem
+              <SidebarItem
                 key={item.href}
                 alert={item.alert}
                 auth={item.auth}
                 href={item.href} 
                 icon={item.icon} 
                 label={item.label}
-            />
+              />
             ))}
             {currentUser && <SidebarItem onClick={() => signOut()} icon={BiLogOut} label="Logout" />}
             <SidebarTweetButton />
+          </div>
         </div>
-        </div>
-    </div>
-)
+      </div>
+  )
 };
 
 export default Sidebar;
