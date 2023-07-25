7361d8d4ed09cf06871bce80e33a4935ab51254d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-25 10:01:02 +0300

    Updated SidebarItem Layout

--- components/Layout/SidebarItem.tsx
+++ components/Layout/SidebarItem.tsx
@@ -7,35 +7,35 @@ import useCurrentUser from '@/hooks/useCurrentUser';
 import { BsDot } from 'react-icons/bs';
 
 interface SidebarItemProps {
-label: string;
-icon: IconType;
-href?: string;
-onClick?: () => void;
-auth?: boolean;
-alert?: boolean;
+  label: string;
+  icon: IconType;
+  href?: string;
+  onClick?: () => void;
+  auth?: boolean;
+  alert?: boolean;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({ label, icon: Icon, href, auth, onClick, alert }) => {
-const router = useRouter();
-const loginModal = useLoginModal();
+  const router = useRouter();
+  const loginModal = useLoginModal();
 
-const { data: currentUser } = useCurrentUser();
+  const { data: currentUser } = useCurrentUser();
 
-const handleClick = useCallback(() => {
+  const handleClick = useCallback(() => {
     if (onClick) {
-    return onClick();
+      return onClick();
     }
 
     if (auth && !currentUser) {
-    loginModal.onOpen();
+      loginModal.onOpen();
     } else if (href) {
-    router.push(href);
+      router.push(href);
     }
-}, [router, href, auth, loginModal, onClick, currentUser]);
+  }, [router, href, auth, loginModal, onClick, currentUser]);
 
-return (
+  return (
     <div onClick={handleClick} className="flex flex-row items-center">
-    <div className="
+      <div className="
         relative
         rounded-full 
         h-14
@@ -48,11 +48,11 @@ return (
         hover:bg-opacity-10 
         cursor-pointer 
         lg:hidden
-    ">
+      ">
         <Icon size={28} color="white" />
         {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
-    </div>
-    <div className="
+      </div>
+      <div className="
         relative
         hidden 
         lg:flex 
@@ -64,15 +64,15 @@ return (
         hover:bg-opacity-10 
         cursor-pointer
         items-center
-    ">
+      ">
         <Icon size={24} color="white" />
         <p className="hidden lg:block text-white text-xl">
-        {label}
+          {label}
         </p>
         {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
+      </div>
     </div>
-    </div>
-);
+  );
 }
 
 export default SidebarItem;
