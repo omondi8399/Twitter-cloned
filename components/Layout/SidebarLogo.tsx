e04c4e9f333c52870338d45139ef38cf0668bf79
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-25 09:59:52 +0300

    Updated SidebarLogo Layout

--- components/Layout/SidebarLogo.tsx
+++ components/Layout/SidebarLogo.tsx
@@ -1,28 +1,27 @@
-import { useRouter } from 'next/router'
-import React from 'react'
-import { BsTwitter } from 'react-icons/bs'
+import { useRouter } from "next/router";
+import { BsTwitter } from "react-icons/bs";
 
 const SidebarLogo = () => {
-    const router = useRouter()
+  const router = useRouter();
+  
   return (
     <div 
-    onClick={() => router.push('/')}
-    className='
-        rounded-full
+      onClick={() => router.push('/')}
+      className="
+        rounded-full 
         h-14
         w-14
-        p-4
+        p-4 
         flex 
-        items-center
-        justify-center
-        hover:bg-blue-300
-        hover: bg-opacity-10
+        items-center 
+        justify-center 
+        hover:bg-blue-300 
+        hover:bg-opacity-10 
         cursor-pointer
-        transition
-    '>
-        <BsTwitter size={20} color="white" />
+    ">
+      <BsTwitter size={28} color="white" />
     </div>
-  )
-}
+  );
+};
 
-export default SidebarLogo
+export default SidebarLogo;
