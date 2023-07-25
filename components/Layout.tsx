7614c1eea0e28b7d71af10fa4658384c97deab97
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-25 10:03:08 +0300

    Updated Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,29 +1,28 @@
-import FollowBar from "./Layout/FollowBar";
-import Sidebar from "./Layout/Sidebar";
+import React from 'react';
 
-interface LayoutProps {
-    children: React.ReactNode;
-}
+import FollowBar from "@/components/layout/FollowBar"
+import Sidebar from "@/components/layout/Sidebar"
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
-        <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-            <div className="grid grid-cols-4 h-full">
-                <Sidebar/>
-                <div className="
-                    col-span-3
-                    lg:col-span-2
-                    border-x-neutral-800
-                ">
-                    {children}
-                </div>
-                <FollowBar />
-            </div>
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div 
+            className="
+              col-span-3 
+              lg:col-span-2 
+              border-x-[1px] 
+              border-neutral-800
+          ">
+            {children}
+          </div>
+          <FollowBar />
         </div>
-
+     </div>
     </div>
   )
 }
 
-export default Layout
+export default Layout;
