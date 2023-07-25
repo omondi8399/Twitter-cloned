33c6d71cf738cbb3c7167020b9c0ebf240371283
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-25 09:56:18 +0300

    Updated useRegisterModal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -10,110 +10,110 @@ import Input from "../Input";
 import Modal from "../Modal";
 
 const RegisterModal = () => {
-const loginModal = useLoginModal();
-const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
 
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [username, setUsername] = useState('');
-const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
 
-const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-const onToggle = useCallback(() => {
+  const onToggle = useCallback(() => {
     if (isLoading) {
-    return;
+      return;
     }
-
+  
     registerModal.onClose();
     loginModal.onOpen();
-}, [loginModal, registerModal, isLoading]);
+  }, [loginModal, registerModal, isLoading]);
 
-const onSubmit = useCallback(async () => {
+  const onSubmit = useCallback(async () => {
     try {
-    setIsLoading(true);
-    
-    await axios.post('/api/register', {
+      setIsLoading(true);
+      
+      await axios.post('/api/register', {
         email,
         password,
         username,
         name,
-    });
+      });
 
-    setIsLoading(false)
+      setIsLoading(false)
 
-    toast.success('Account created.');
+      toast.success('Account created.');
 
-    signIn('credentials', {
+      signIn('credentials', {
         email,
         password,
-    });
+      });
 
-    registerModal.onClose();
+      registerModal.onClose();
     } catch (error) {
-    toast.error('Something went wrong');
+      toast.error('Something went wrong');
     } finally {
-    setIsLoading(false);
+      setIsLoading(false);
     }
-}, [email, password, registerModal, username, name]);
+  }, [email, password, registerModal, username, name]);
 
-const bodyContent = (
+  const bodyContent = (
     <div className="flex flex-col gap-4">
-    <Input
+      <Input
         disabled={isLoading}
         placeholder="Email" 
-        value={email}   
+        value={email} 
         onChange={(e) => setEmail(e.target.value)} 
-    />
-    <Input 
+      />
+      <Input 
         disabled={isLoading}
         placeholder="Name" 
         value={name} 
         onChange={(e) => setName(e.target.value)} 
-    />
-    <Input 
+      />
+      <Input 
         disabled={isLoading}
         placeholder="Username" 
         value={username} 
         onChange={(e) => setUsername(e.target.value)}
-    />
-    <Input 
+      />
+      <Input 
         disabled={isLoading}
         placeholder="Password" 
         type="password" 
         value={password} 
         onChange={(e) => setPassword(e.target.value)}
-    />
+      />
     </div>
-)
+  )
 
-const footerContent = (
+  const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
-    <p>Already have an account?
+      <p>Already have an account?
         <span 
-        onClick={onToggle} 
-        className="
+          onClick={onToggle} 
+          className="
             text-white 
             cursor-pointer 
             hover:underline
-        "
-        > Sign in</span>
-    </p>
+          "
+          > Sign in</span>
+      </p>
     </div>
-)
+  )
 
-return (
+  return (
     <Modal
-    disabled={isLoading}
-    isOpen={registerModal.isOpen}
-    title="Create an account"
-    actionLabel="Register"
-    onClose={registerModal.onClose}
-    onSubmit={onSubmit}
-    body={bodyContent}
-    footer={footerContent}
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Create an account"
+      actionLabel="Register"
+      onClose={registerModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+      footer={footerContent}
     />
-);
+  );
 }
 
 export default RegisterModal;
